4793f38c55854bb812028c7099e6fe8589ee2278
Sergey Nesterov <nesterov_s_u@mail.ru>
2021-05-18 13:49:14 +0500

    Adding @RequestMapping("first")

--- src/main/Java/ru/nesterov/app1/controllers/FirstController.java
+++ src/main/Java/ru/nesterov/app1/controllers/FirstController.java
@@ -2,12 +2,14 @@ package ru.nesterov.app1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Sergey Nesterov
  */
 
 @Controller
+@RequestMapping("first")
 public class FirstController {
     @GetMapping("h")
     public String helloPage(){
